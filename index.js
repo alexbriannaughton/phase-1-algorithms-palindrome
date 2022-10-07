46c675828e7be7142d16f69509934c54064a13e4
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-10-07 13:38:30 -0700

    commit tree a42b957ba85c3fe8ad77192998bbe7114394168c

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if (word === reverse(word)){
+    return true
+} else {
+  return false
+}}
+function reverse(word) {
+  return word.split("").reverse().join("")
 }
-
 /* 
   Add your pseudocode here
 */
